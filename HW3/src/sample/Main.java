09f95ce3392ab83a05a08b94c4d376be259a8f74
Ain-Joonas Toose <Ain.Joonas.Toose@gmail.com>
2014-11-16 03:43:01 +0200

    -A few test projects for future reference. -Huge additions and fixes to HW3. Broken rectangle builder, working line follower with dot. Saving, menu functions, menu fixes, canvas moving(first attempt at moving objects), clearing function.

--- HW3/src/sample/Main.java
+++ HW3/src/sample/Main.java
@@ -1,47 +1,51 @@
 package sample;
-
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
     public void start(final Stage primaryStage) {
 
-        Canvas canvas = new Canvas(400, 400);
-        final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        initDraw(graphicsContext);
-
-        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED,
-                new EventHandler<MouseEvent>(){
-
-                    @Override
-                    public void handle(MouseEvent event) {
-                        graphicsContext.beginPath();
-                        graphicsContext.moveTo(event.getX(), event.getY());
-                        graphicsContext.stroke();
-                    }
-                });
+        final Canvas canvas = new Canvas(400,400);
+        canvas.setStyle("-fx-background: rgba(0, 30, 0, 0);");
+        final GraphicsContext g = canvas.getGraphicsContext2D();
+        initDraw(g);
 
         canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED,
-                new EventHandler<MouseEvent>(){
-
+                new EventHandler<MouseEvent>() {
                     @Override
                     public void handle(MouseEvent event) {
-                        graphicsContext.lineTo(event.getX(), event.getY());
-                        graphicsContext.stroke();
+                        if(event.isSecondaryButtonDown()){
+                            double x = event.getSceneX();
+                            double y = event.getSceneY();
+                            moveCanvas(x,y, g);
+                            System.out.println("RIGHT MOUSE BTN DOWN");
+                        }
                     }
                 });
 
@@ -51,22 +55,79 @@ public class Main extends Application {
                     @Override
                     public void handle(MouseEvent event) {
 
+
                     }
                 });
 
         MenuBar menuBar = new MenuBar();
         Menu menuFile = new Menu("File");
         Menu menuView = new Menu("View");
+        Menu menuFunct = new Menu("Functions");
+        MenuItem Rect = new MenuItem("Rectangle");
+        Rect.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                final GraphicsContext g = canvas.getGraphicsContext2D();
+                Rect(g);
+            }
+        });
+        MenuItem Line = new MenuItem("Line");
+        Line.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                final GraphicsContext g = canvas.getGraphicsContext2D();
+                Line(g);
+            }
+        });
         MenuItem menuFileSave = new MenuItem("Save");
         menuBar.getMenus().add(menuFile);
-        menuBar.getMenus().add(menuView);
         menuFile.getItems().add(menuFileSave);
+        menuFileSave.setOnAction(new EventHandler<javafx.event.ActionEvent>() {
+            @Override
+            public void handle(javafx.event.ActionEvent event) {
+                FileChooser fc = new FileChooser();
+                fc.setInitialDirectory(new File("res/maps"));
+                fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG","*.png"));
+                fc.setTitle("Save image");
+                File file = fc.showSaveDialog(primaryStage);
+                if(file != null){
+                    Double CanvasDoubleValueWidth = canvas.getWidth();
+                    Double CanvasDoubleValueHeight = canvas.getHeight();
+                    Integer CanvasIntValueWidth = CanvasDoubleValueWidth.intValue();
+                    Integer CanvasIntValueHeight = CanvasDoubleValueHeight.intValue();
+                    WritableImage wi = new WritableImage(CanvasIntValueWidth, CanvasIntValueWidth);
+                    try {
+                        SnapshotParameters sp = new SnapshotParameters();
+                        sp.setFill(Color.TRANSPARENT);
+                        ImageIO.write(SwingFXUtils.fromFXImage(canvas.snapshot(sp, wi), null), "png", file);
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+        menuBar.getMenus().add(menuFunct);
+        menuFunct.getItems().add(Rect);
+        menuFunct.getItems().add(Line);
+
 
         BorderPane root = new BorderPane();
 
         root.getChildren().add(canvas);
         root.setTop(menuBar);
         final Scene scene = new Scene(root, 400, 400);
+        scene.addEventHandler(KeyEvent.KEY_PRESSED,
+                new EventHandler<KeyEvent>() {
+                    @Override
+                    public void handle(KeyEvent event) {
+                        if(event.getCode() == KeyCode.BACK_SPACE){
+                            canvasReset(g);
+                            System.out.println("BACKSPACE PRESSED");
+                        }
+                    }
+                });
         scene.addEventHandler(KeyEvent.KEY_PRESSED,
                 new EventHandler<KeyEvent>(){
                     @Override
@@ -92,17 +153,73 @@ public class Main extends Application {
         double canvasWidth = gc.getCanvas().getWidth();
         double canvasHeight = gc.getCanvas().getHeight();
 
-        gc.fill();
-        gc.strokeRect(
-                0,              //x == upper left corner
-                0,              //y == upper left corner
-                canvasWidth,    //width
-                canvasHeight);  //height
-
-        gc.setFill(Color.RED);
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
         gc.setStroke(Color.GREEN);
         gc.setLineWidth(1);
 
+
+    }
+    private void canvasReset(GraphicsContext gc){
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+    }
+    private void moveCanvas(double x, double y, GraphicsContext gc){
+        gc.getCanvas().setTranslateX(x);
+        gc.getCanvas().setTranslateY(y);
+
+    }
+    public void Rect(final GraphicsContext gc){
+
+        EventHandler<MouseEvent> mouseHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if(event.getEventType() == MouseEvent.MOUSE_PRESSED){
+                    gc.strokeRect(event.getX(),event.getY(),event.getX(), event.getY());
+                }
+                else  if(event.getEventType() == MouseEvent.MOUSE_DRAGGED){
+                    gc.strokeRect(event.getX(),event.getY(),event.getX(),event.getY());
+                }
+
+            }
+        };
+    }
+    public void Line(final GraphicsContext gc){
+        gc.getCanvas().addEventHandler(MouseEvent.MOUSE_PRESSED,
+                new EventHandler<MouseEvent>() {
+
+                    @Override
+                    public void handle(MouseEvent event) {
+                        gc.beginPath();
+                        gc.moveTo(event.getX(), event.getY());
+                        gc.stroke();
+                    }
+                });
+        gc.getCanvas().addEventHandler(MouseEvent.MOUSE_DRAGGED,
+                new EventHandler<MouseEvent>() {
+
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if (event.isPrimaryButtonDown()) {
+                            gc.setLineWidth(1);
+                            gc.lineTo(event.getX(), event.getY());
+                            gc.stroke();
+                            System.out.println(event.getX() + event.getY());
+                        }
+
+                    }
+                });
+        gc.getCanvas().addEventHandler(MouseEvent.MOUSE_CLICKED,
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        gc.setLineWidth(5);
+                        gc.moveTo(event.getX(), event.getY());
+                        gc.lineTo(event.getX(), event.getY());
+                        gc.stroke();
+                        System.out.println("MOUSECLICK;" + "X:" + event.getX() + " " + "Y:" + event.getY());
+                    }
+                });
     }
 
 }
